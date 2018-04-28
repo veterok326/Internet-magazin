1f98d36208917fa639d9ce4047c23b7abfe44571
veterok326 <necrovit4@gmail.com>
2018-04-28 08:37:06 +0300

    Lesser changes with Yii-2 framework project(1)

---
+++ common/bootstrap/Setup.php
@@ -0,0 +1,10 @@
+<?php
+namespace common\bootstrap;
+use yii\base\BootstrapInterface;
+class Setup implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        // TODO: Implement bootstrap() method.
+    }
+}
